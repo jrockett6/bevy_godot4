5405c81fb7cf695b53f199bd911d2ceda8c6e160
Udolph Hartholtz <udolph.hartholtz@gmx.de>
2024-09-08 13:37:04 +0200

    fix BevyApp not being initialized outside editor

--- proc_macros/src/lib.rs
+++ proc_macros/src/lib.rs
@@ -12,7 +12,7 @@ pub fn bevy_app(_attr: TokenStream, item: TokenStream) -> TokenStream {
         #[gdextension]
         unsafe impl ExtensionLibrary for BevyExtensionLibrary {
             fn on_level_init(level: godot::prelude::InitLevel) {
-                if level == godot::prelude::InitLevel::Editor {
+                if level == godot::prelude::InitLevel::Core {
                     godot::private::class_macros::registry::class::auto_register_classes(level);
                     let mut app_builder_func = bevy_godot4::APP_BUILDER_FN.lock().unwrap();
                     if app_builder_func.is_none() {
